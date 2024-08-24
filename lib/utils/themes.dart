294f7deec7276fc770ebe6ef272249e1c7ee6224
Vinícius Inácio <51095026+ViniciusInacio0108@users.noreply.github.com>
2024-08-23 21:47:32 -0300

    feat: added themes

--- lib/utils/themes.dart
+++ lib/utils/themes.dart
@@ -2,6 +2,17 @@ import 'package:flutter/material.dart';
 
 class MyThemeData {
   static ThemeData themeData() {
-    return ThemeData();
+    return ThemeData(
+      colorScheme: ColorScheme(
+          brightness: Brightness.light,
+          primary: const Color.fromRGBO(11, 19, 43, 1),
+          onPrimary: const Color.fromRGBO(28, 37, 65, 1),
+          secondary: Colors.white,
+          onSecondary: Colors.white.withOpacity(0.5),
+          error: Colors.red,
+          onError: Colors.red.withOpacity(0.5),
+          surface: const Color.fromRGBO(58, 80, 107, 1),
+          onSurface: const Color.fromRGBO(91, 192, 190, 1)),
+    );
   }
 }
