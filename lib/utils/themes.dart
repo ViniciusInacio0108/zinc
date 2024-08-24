0c4038911fbcd3601b64f5047ea17e7987e5aee2
Vinícius Inácio <51095026+ViniciusInacio0108@users.noreply.github.com>
2024-08-24 14:00:11 -0300

    feat: added theme data

--- lib/utils/themes.dart
+++ lib/utils/themes.dart
@@ -3,16 +3,28 @@ import 'package:flutter/material.dart';
 class MyThemeData {
   static ThemeData themeData() {
     return ThemeData(
+      bottomSheetTheme: const BottomSheetThemeData(
+        dragHandleColor: const Color.fromRGBO(11, 19, 43, 1),
+      ),
       colorScheme: ColorScheme(
-          brightness: Brightness.light,
-          primary: const Color.fromRGBO(11, 19, 43, 1),
-          onPrimary: const Color.fromRGBO(28, 37, 65, 1),
-          secondary: Colors.white,
-          onSecondary: Colors.white.withOpacity(0.5),
-          error: Colors.red,
-          onError: Colors.red.withOpacity(0.5),
-          surface: const Color.fromRGBO(58, 80, 107, 1),
-          onSurface: const Color.fromRGBO(91, 192, 190, 1)),
+        brightness: Brightness.light,
+        primary: const Color.fromRGBO(11, 19, 43, 1),
+        onPrimary: const Color.fromRGBO(28, 37, 65, 1),
+        secondary: Colors.white,
+        onSecondary: Colors.white.withOpacity(0.5),
+        error: Colors.red,
+        onError: Colors.red.withOpacity(0.5),
+        surface: const Color.fromRGBO(58, 80, 107, 1),
+        onSurface: const Color.fromRGBO(91, 192, 190, 1),
+      ),
+      textTheme: TextTheme(
+        bodyMedium: const TextStyle().copyWith(
+          color: const Color.fromRGBO(11, 19, 43, 1),
+        ),
+        titleMedium: const TextStyle().copyWith(
+          color: const Color.fromRGBO(11, 19, 43, 1),
+        ),
+      ),
     );
   }
 }
